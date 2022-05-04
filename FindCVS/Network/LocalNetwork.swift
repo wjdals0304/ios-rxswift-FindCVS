e3490f4b072dd92543acc5f5a1a238f7341b720a
jmkim <wjdals0304@gmail.com>
2022-05-04 22:36:20 +0900

    add: API file add

---
+++ FindCVS/Network/LocalNetwork.swift
@@ -0,0 +1,41 @@
+//
+//  LocalNetwork.swift
+//  FindCVS
+//
+//  Created by 김정민 on 2022/05/04.
+//
+
+import RxSwift
+import UIKit
+
+final class LocalNetwork {
+    private let session: URLSession
+    let api = LocalAPI()
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func getLocation(by mapPoint: MTMapPoint) -> Single<Result<LocationData, URLError>> {
+        guard let url = api.getLocation(by: mapPoint).url else {
+            return .just(.failure(URLError(.badURL)))
+        }
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK d85d835f5b31f206840ccaeddd829f2a", forHTTPHeaderField: "Authorization")
+        
+        return session.rx.data(request: request as URLRequest)
+            .map { data in
+                do {
+                    let locationData = try JSONDecoder().decode(LocationData.self, from: data)
+                    return .success(locationData)
+                } catch {
+                    return .failure(URLError(.cannotParseResponse))
+                }
+            }
+            .catch { _ in .just(Result.failure(URLError(.cannotLoadFromNetwork))) }
+            .asSingle()
+    }
+}
+
